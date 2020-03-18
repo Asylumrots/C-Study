52ea129b0968eee40464c2a7708fd7020f7fd859
Asylumrots <1773527072@qq.com>
2020-03-18 13:49:07 +0800

    second

--- C1/0706后面函数/15字符串的类型转换.c
+++ C1/0706后面函数/15字符串的类型转换.c
@@ -9,7 +9,7 @@ int main()
 	//忽略空格从正负号开始查找数字 到不是数字或\0停止 
 	//数字前有别的则停止 返回0 即错误 " asd 123我拷贝"
 	int n = atoi(arr);
-	printf("%d\n", n);
+	printf("%d\n", n); 
 
 	char arr2[] = " 123.234我是12322.223";
 	float f = atof(arr2);
